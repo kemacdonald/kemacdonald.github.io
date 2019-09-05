5aef88518133c412d0726c91d61328e74b6dc31f
Kyle MacDonald <kylem412@gmail.com>
2019-09-04 22:57:25 -0700

    final update to cat sampler

--- experiments/soc_info_cat/js/cat_init.js
+++ experiments/soc_info_cat/js/cat_init.js
@@ -5,7 +5,10 @@ function init() {
   //                           "activation-social", "activation-no_social",
   //                           "presentation-social", "presentation-no_social",
   //                           "no_goal-social", "no_goal-no_social"]);
-  exp.condition = _.sample(["no_goal-no_social"]);
+  exp.condition = _.sample(["learning-no_social", 
+                            "activation-no_social",
+                            "presentation-no_social",
+                            "no_goal-social"]);
   exp.goal_condition = extract_goal_condition(exp.condition);
   exp.social_condition = extract_soc_condition(exp.condition);
   exp.img_keys = _.shuffle(["cube-spinner.png", "cyl-lever.png", "tri-button.png"]);
